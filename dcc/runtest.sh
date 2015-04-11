6e316aa48f01056fb7348efb6c75e95ae32fe5b8
Rick Weyrauch <rpweyrauch@gmail.com>
2015-04-11 08:12:57 -0700

    Add more test programs.

--- dcc/runtest.sh
+++ dcc/runtest.sh
@@ -36,3 +36,30 @@ do
     ${DCC} -o out/$dcfinput.s $input
     gcc out/$dcfinput.s -o out/$dcfinput.out 2> out/$dcfinput.log
 done
+
+echo "Running dcc optimizations tests."
+
+TESTFILES=testdata/optimizer/correctness/*.dcf
+for input in ${TESTFILES}
+do
+    dcfinput=${input##*/}
+    echo $dcfinput
+    ${DCC} -o out/$dcfinput.s $input
+    gcc out/$dcfinput.s -o out/$dcfinput.out 2> out/$dcfinput.log
+done
+
+echo "Running dcc optimization performance tests."
+
+TESTFILES=testdata/optimizer/tests/*.dcf
+gcc -c testdata/optimizer/tests/lib/6035.c -o out/6035.o
+for input in ${TESTFILES}
+do
+    dcfinput=${input##*/}
+    echo $dcfinput
+    ${DCC} -o out/$dcfinput.s $input
+    gcc out/$dcfinput.s out/6035.o -o out/$dcfinput.out 2> out/$dcfinput.log
+done
+cp testdata/optimizer/tests/saman.pgm out
+cp testdata/optimizer/tests/saman_noise.pgm out
+cp testdata/optimizer/tests/segovia.pgm out
+
